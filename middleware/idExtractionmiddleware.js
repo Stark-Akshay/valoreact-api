8c7cba5c20aec023a35273662e3e90c143d478e5
Stark-Akshay <akshaymanoj.dms@gmail.com>
2025-01-02 11:42:36 +0000

    error message changed for yt url

--- middleware/idExtractionmiddleware.js
+++ middleware/idExtractionmiddleware.js
@@ -5,9 +5,11 @@ const extractVideoID = (req, res, next) => {
   const { url } = req.body;
   const videoID = extractID(url);
   if (!videoID) {
-    return res
-      .status(400)
-      .json({ success: false, message: "URL is not a YouTube link" });
+    return res.status(400).json({
+      success: false,
+      message:
+        "Please provide the full Youtube URL. Eg: https://www.youtube.com/watch?v=fUsaDm9nNkY",
+    });
   }
   req.videoID = videoID; // Storing the extracted ID for the next middleware
   next();
