c5108a1bd711e91d217f564f1e091590ef4da181
Stark-Akshay <akshaymanoj.dms@gmail.com>
2025-01-14 08:07:34 +0000

    video id extraction algo changed

--- controllers/checkingController.js
+++ controllers/checkingController.js
@@ -3,8 +3,14 @@ const { verifyVideo } = require("../utils/verifyVideo");
 const Video = require("../models/videoModel");
 
 const saveVideo = async (req, res) => {
+  const data = {
+    url: req.videoID,
+    rank: req.body.rank,
+    riotID: req.body.riotID,
+  };
+  // console.log(data);
   try {
-    const video = await Video.create(req.body);
+    const video = await Video.create(data);
     res.status(200).json(video);
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
--- utils/extractID.js
+++ utils/extractID.js
@@ -1,11 +1,20 @@
 //checking url is youtube if yes extract ID
 
 const extractID = (url) => {
+  // var regExp =
+  //   /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+  // var match = url.match(regExp);
+  // if (url.includes("youtube.com") && match && match[2].length == 11) {
+  //   return match[2];
+  // }
+
   var regExp =
-    /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
   var match = url.match(regExp);
-  if (url.includes("youtube.com") && match && match[2].length == 11) {
-    return match[2];
+  // console.log(`original url: ${url}`);
+  // console.log(`parsed url: ${match[7]}`);
+  if (match && match[7].length == 11) {
+    return match[7];
   } else {
     return false;
   }
